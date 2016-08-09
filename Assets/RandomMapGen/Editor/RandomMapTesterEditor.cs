7b5e899b6e9d4de790cd2d3e3ecf1737e852d84b
Scott Bromander <scott@primeacademy.io>
2016-08-08 21:50:23 -0500

    makes tiles wo sprites

---
+++ Assets/RandomMapGen/Editor/RandomMapTesterEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+[CustomEditor(typeof(RandomMapTester))]
+public class NewBehaviourScript : Editor {
+
+	public override void OnInspectorGUI(){
+		DrawDefaultInspector ();
+		var script = (RandomMapTester)target;
+
+		if(GUILayout.Button("Generate Island")){
+			if (Application.isPlaying) {
+				script.MakeMap ();
+			}
+		}
+	}
+}
--- Assets/RandomMapGen/Scripts/RandomMapTester.cs
+++ Assets/RandomMapGen/Scripts/RandomMapTester.cs
@@ -7,6 +7,13 @@ public class RandomMapTester : MonoBehaviour {
 	public int mapWidth = 20;
 	public int mapHeight = 20;
 
+	[Space]
+	[Header ("Visualize Map")]
+	public GameObject mapContainer;
+	public GameObject tilePrefab;
+	public Vector2 tileSize = new Vector2 (16, 16);
+
+
 	public Map map;
 
 	// Use this for initialization
@@ -17,5 +24,28 @@ public class RandomMapTester : MonoBehaviour {
 	public void MakeMap(){
 		map.NewMap (mapWidth, mapHeight);
 		Debug.Log ("Created a new map " + map.columns + "x" + map.rows + " map.");
+		CreateGrid ();
+	}
+
+	void CreateGrid(){
+		var total = map.tiles.Length;
+		var maxColumns = map.columns;
+		var column = 0;
+		var row = 0;
+
+		for (var i = 0; i < total; i++) {
+			column = i % maxColumns;
+			var newX = column * tileSize.x;
+			var newY = -row * tileSize.y;
+
+			var go = Instantiate (tilePrefab);
+			go.name = "Tile " + i;
+			go.transform.SetParent (mapContainer.transform);
+			go.transform.position = new Vector3 (newX, newY, 0);
+
+			if (column == (maxColumns - 1)) {
+				row++;
+			}
+		}
 	}
 }
