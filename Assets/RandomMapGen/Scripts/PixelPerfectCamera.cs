f1f4dbccdd7e0be9bcadcdf4afb12f7d113c61e9
Scott Bromander <scott@primeacademy.io>
2016-08-08 22:09:38 -0500

    camera fix

---
+++ Assets/RandomMapGen/Scripts/PixelPerfectCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelPerfectCamera : MonoBehaviour {
+
+	public static float pixelsToUnits = 1f;
+	public static float scale = 1f;
+	public Vector2 nativeResolution = new Vector2(160, 144);
+
+	void Awake(){
+		var camera = GetComponent<Camera> ();
+		if (camera.orthographic) {
+			var dir = Screen.height;
+			var res = nativeResolution.y;
+
+			scale = dir / res;
+			pixelsToUnits *= scale;
+
+			camera.orthographicSize = (dir / 2.0f) / pixelsToUnits;
+		}
+	}
+}
