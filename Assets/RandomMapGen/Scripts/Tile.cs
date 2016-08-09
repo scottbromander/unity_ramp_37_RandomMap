8359a674503f1d5fe4b3c212b0c50d78f8907ac2
Scott Bromander <scott@primeacademy.io>
2016-08-08 21:19:15 -0500

    tile class setup

---
+++ Assets/RandomMapGen/Scripts/Tile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Sides{
+	Bottom,
+	Right,
+	Left,
+	Top
+}
+
+public class Tile {
+	public int id = 0;
+	public Tile[] neighbors = new Tile[4];
+
+}
