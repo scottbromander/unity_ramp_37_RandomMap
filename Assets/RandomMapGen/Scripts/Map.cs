48b1fc374b87e5fbf37010472871f100e3fac44b
Scott Bromander <scott@primeacademy.io>
2016-08-11 15:53:29 -0500

    foundation for decoration of tiles

--- Assets/RandomMapGen/Scripts/Map.cs
+++ Assets/RandomMapGen/Scripts/Map.cs
@@ -1,6 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum TileType {
+	Empty = -1,
+	Grass = 15,
+
+
+}
+
 public class Map {
 
 	public Tile[] tiles;
@@ -53,6 +60,19 @@ public class Map {
 					tile.AddNeighbor (Sides.Top, tiles [columns * (r - 1) + c]);
 				}
 			}
+		}
+	}
+
+	public void DecorateTiles(Tile[] tiles, float percent, TileType type){
+		var total = Mathf.FloorToInt (tiles.Length * percent);
+		for (var i = 0; i < total; i++) {
+			var tile = tiles [i];
+
+			if (type == TileType.Empty) {
+				tile.ClearNeighbors ();
+			}
+
+			tile.autoTileID = (int)type;
 		}
 	}
 }
